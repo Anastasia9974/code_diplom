996b337da40136d415e65c4b5c5ecde192b89d6a
AnVi <vasilieva7.aa@edu.spbstu.ru>
2025-06-23 14:10:07 +0300

    23_06

--- federated_learning/NN/model.py
+++ federated_learning/NN/model.py
@@ -6,21 +6,20 @@ class CNN():
         self.model = Sequential()
 
         # Conv1
-        self.model.add(layers.Conv2D(32, kernel_size=3, activation='relu', input_shape=input_shape))
+        self.model.add(layers.Conv2D(6, kernel_size=5, activation='relu', input_shape=input_shape))
         self.model.add(layers.MaxPooling2D(pool_size=2))
 
         # Conv2
-        self.model.add(layers.Conv2D(64, kernel_size=3, activation='relu'))
+        self.model.add(layers.Conv2D(16, kernel_size=5, activation='relu'))
         self.model.add(layers.MaxPooling2D(pool_size=2))
 
-        self.model.add(layers.Conv2D(64, kernel_size=3, activation='relu'))
         # Flatten
         self.model.add(layers.Flatten())
 
         # Fully connected layers
-        self.model.add(layers.Dense(64, activation='relu'))
-
-        self.model.add(layers.Dense(10))
+        self.model.add(layers.Dense(120, activation='relu'))
+        self.model.add(layers.Dense(84, activation='relu'))
+        self.model.add(layers.Dense(10))  # Логиты, softmax добавляется позже при компиляции
 
     def show_arhitecture(self):
         print(self.model.summary())
